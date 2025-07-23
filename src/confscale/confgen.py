b6f49e9a625c4146a8b244837871e7993ae6281a
Etienne Reboul <reboul@ibpc.fr>
2025-07-23 15:49:00 +0200

    Refactor embeding3D_wrapper to handle None seed and improve documentation

--- src/confscale/confgen.py
+++ src/confscale/confgen.py
@@ -15,6 +15,8 @@ Examples:
     >>> mol_with_conformers = embeding3D_wrapper(mol, nb_conformers=10, seed=42)
 """
 
+import time
+
 from rdkit import Chem
 from rdkit.Chem import rdDistGeom
 
@@ -23,11 +25,20 @@ def smi2molh(smi: str) -> Chem.Mol:
     """
     Convert a SMILES string to an RDKit Mol object with hydrogens added.
 
-    Parameters:
-    smi (str): The SMILES string to convert.
+    Parameters
+    ----------
+    smi : str
+        The SMILES string to convert.
 
-    Returns:
-    Chem.Mol: The RDKit Mol object with hydrogens added.
+    Returns
+    -------
+    Chem.Mol
+        The RDKit Mol object with hydrogens added.
+
+    Raises
+    ------
+    ValueError
+        If the SMILES string is invalid.
     """
     mol = Chem.MolFromSmiles(smi)
     if mol is None:
@@ -36,22 +47,38 @@ def smi2molh(smi: str) -> Chem.Mol:
     return mol
 
 
-def embeding3D_wrapper(molh: Chem.Mol, nb_conformers: int, seed: int, nb_thread: int = 2, forcetol: float = 0.0135) -> Chem.Mol:
+def embeding3D_wrapper(
+    molh: Chem.Mol, nb_conformers: int, seed: int | None = None, nb_thread: int = 2, forcetol: float = 0.0135
+) -> Chem.Mol:
     """
-    Generate 3D conformers for a given RDKit Mol object.
+    Generate 3D conformers for a given RDKit Mol object
+    Parameters
+    ----------
+    molh : Chem.Mol
+        The RDKit Mol object with hydrogens added.
+    nb_conformers : int
+        The number of conformers to generate.
+    seed : int or None, optional
+        Random seed for reproducibility. If None, a time-based seed will be generated.
+    nb_thread : int, default=2
+        Number of threads to use for conformer generation.
+    forcetol : float, default=0.0135
+        Force tolerance for the embedding algorithm.
 
-    Parameters:
-    molh (Chem.Mol): The RDKit Mol object with hydrogens added.
-    nb_conformers (int): The number of conformers to generate.
-    seed (int): Random seed for reproducibility.
-    nb_thread (int): Number of threads to use for conformer generation.
-    forcetol (float): Force tolerance for the embedding algorithm.
+    Returns
+    -------
+    Chem.Mol
+        The RDKit Mol object with generated 3D conformers.
 
-    Returns:
-    Chem.Mol: The RDKit Mol object with generated 3D conformers.
+    Notes
+    -----
+        - When seed is not provided, a time-based seed is generated using the current time
+        multiplied by 1,000,000 and modulo 2^31 to ensure it fits within the range of a 32-bit
+        signed integer, in order to avoid potential issues with large integers.
+        - The `forcetol` parameter is set to 0.0135, in order
     """
     etkdg = rdDistGeom.ETKDGv3()
-    etkdg.randomSeed = seed
+    etkdg.randomSeed = seed if seed is not None else int(time.time() * 1000000) % (2**31)
     etkdg.verbose = False
     etkdg.numThreads = nb_thread
     etkdg.useRandomCoords = True
